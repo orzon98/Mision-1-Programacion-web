42a3555eb0607b482daa28189f2c0274834fbbd6
orzon98 <124667445+orzon98@users.noreply.github.com>
2026-09-17 11:22:04 +0200

    Estructura y decorado
    Este primer commit es del html y css del juego, de momento no hay nada funcional.

---
+++ app.js
@@ -0,0 +1,17 @@
+let btn_start = document.getElementById("start");
+
+let green = document.getElementById("verde");
+let red = document.getElementById("rojo");
+let yellow = document.getElementById("amarillo");
+let blue = document.getElementById("azul");
+
+let nivel = document.getElementById("nivel");
+
+let cont = 0;
+
+function empezar(){
+    if(nivel != 0) {
+        nivel.innerHTML=0;
+    }
+    
+}
