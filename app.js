d54531d9bd30696ddc915f9ef74b603935fa7aab
Daniel Lopez <daloval8590@gmail.com>
2026-09-17 19:51:03 +0200

    Lógica de la maquina
    He implementado una parte de la logica de la maquina para elegir colores de forma aleatoria.
    
    Falta añadir colores por nivel y implementar la parte del jugador.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let btn_simon = document.getElementById("titulo");
 let btn_start = document.getElementById("start");
 
-let green = document.getElementById("verde");
-let red = document.getElementById("rojo");
-let yellow = document.getElementById("amarillo");
-let blue = document.getElementById("azul");
+let verde = document.getElementById("verde");
+let rojo = document.getElementById("rojo");
+let amarillo = document.getElementById("amarillo");
+let azul = document.getElementById("azul");
 
 let nivel = document.getElementById("nivel");
 let letras_nivel = document.getElementById("textoNivel")
@@ -14,12 +14,13 @@ let modoOscuroActivo = false;
 
 //Funcion para resetear el nivel y para empezar el juego
 function empezar(){
-    if(nivel != 0) {
-        nivel.innerHTML=0;
-    }
-    
+    nivel.innerHTML=0;
+    cont = 0;
+    secuencia = [];
+    siguienteNivel();
 }
 
+//Funcion para activar y desactivar el modo oscuro
 function modoOscuro(){
     if(!modoOscuroActivo){
         document.body.style.backgroundImage = "url('/img/fondo_oscuro.jpg')";
@@ -33,4 +34,47 @@ function modoOscuro(){
         letras_nivel.style.color = "#000";
     }
     modoOscuroActivo = !modoOscuroActivo;
+}
+
+let colores = [rojo, verde, amarillo, azul];
+
+function encenderBoton(color){
+    color.classList.add("activo");
+    setTimeout(function(){
+        color.classList.remove("activo")
+    }, 500);
+}
+
+let intervalo;
+
+function reproducirSecuencia(){
+    let i = 0;
+    intervalo = setInterval(function(){
+        encenderBoton(secuencia[i]);
+        i++;
+        if(i >= secuencia.length){
+            clearInterval(intervalo)
+        }
+    }, 900);
+}
+
+let secuencia = [];
+function obtenerColor(){
+    return Math.floor(Math.random() * colores.length);
+}
+
+function siguienteNivel(){
+    cont++;
+    nivel.innerHTML=cont;
+
+    let indiceAleatorio = obtenerColor();
+    let colorSeleccionado = colores[indiceAleatorio];
+
+    secuencia.push(colorSeleccionado);
+
+    console.log("Color añadido:", colorSeleccionado.id);
+    console.log("Secuencia actual:", secuencia.map(b => b.id));
+
+    reproducirSecuencia();
+
 }
