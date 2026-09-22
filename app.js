0053c1b6747cd61fe24c1d89a452be33b1a53ef8
Daniel Lopez <daloval8590@gmail.com>
2026-09-22 16:22:20 +0200

    Comentarios

--- app.js
+++ app.js
@@ -36,24 +36,35 @@ function modoOscuro(){
     modoOscuroActivo = !modoOscuroActivo;
 }
 
+//Variable para guardar los colores
 let colores = [rojo, verde, amarillo, azul];
 
+//Funcion para encender un boton
 function encenderBoton(color){
+    //Se añade el color a la clase activo que contiene propiedades para que resalte el color seleccionado
     color.classList.add("activo");
+
+    //Se le aplica un timeout de 500ms para quitarle esa clase y vuelva a su estado normal
     setTimeout(function(){
         color.classList.remove("activo")
     }, 500);
 }
 
+
 let intervalo;
 let puedeJugar;
 
+//Funcion que reproduce la secuencia de colores
 function reproducirSecuencia(){
     let i = 0;
+    //Mientras se reproduce la secuencia, el jugador no puede jugar (se bloquea con puedeJugar)
     puedeJugar = false;
+
+    //Enciende todos los botones de la secuencia actual y espera 900ms para avanzar de nivel
     intervalo = setInterval(function(){
         encenderBoton(secuencia[i]);
         i++;
+        //Cuando i llega a la longitud de secuencia significa que ya se ha llegado al final por lo que puede empezar a jugar el jugador
         if(i >= secuencia.length){
             clearInterval(intervalo);
             puedeJugar = true;
@@ -62,49 +73,64 @@ function reproducirSecuencia(){
 }
 
 let secuencia = [];
+
+//Funcion para obtener un color de forma aleatoria
 function obtenerColor(){
     return Math.floor(Math.random() * colores.length);
 }
 
 let secuenciaJugador = [];
 
+//Funcion para avanzar de nivel
 function siguienteNivel(){
     cont++;
     nivel.innerHTML=cont;
+
     secuenciaJugador = [];
 
+    //Se elige un color aleatorio y se introduce en el array secuencia
     let indiceAleatorio = obtenerColor();
     let colorSeleccionado = colores[indiceAleatorio];
 
     secuencia.push(colorSeleccionado);
 
+    //Por ultimo se llama a la funcion de reproducir secuencia para que ilumine los colores
     reproducirSecuencia();
 }
 
+//Funcion para implementar la jugabilidad del usuario
 function jugadorPulsa(color){
+    //Si la maquina sigue reproduciendo la secuencia se ignoran los clicks
     if(!puedeJugar) return;
 
+    //Cuando el jugador pulsa llamamos a encenderBoton para que se vea cual ha pulsado
     encenderBoton(color);
+    //Se mete el color en el array del usuario 
     secuenciaJugador.push(color);
 
+    //Variable que guarda el indice del ultimo elemento que el jugador acaba de añadir a secuenciaJugador
     let posicion = secuenciaJugador.length - 1;
 
+    //Se comprueba si el color que ha pulsado el jugador es el mismo que el de la secuencia y sino pierde
     if(secuenciaJugador[posicion] !== secuencia[posicion]){
         perder();
         return;
     }
 
+    //Si la longitud es la misma se avanza de nivel
     if(secuenciaJugador.length === secuencia.length){
         puedeJugar = false;
         setTimeout(siguienteNivel, 1000);
     }
 }
 
+//Funcion para mandar una alerta cuando se pierde
 function perder(){
     puedeJugar = false;
     alert("Has perdido. Llegaste al nivel: " + cont);
 }
 
+//Se asocia a cada boton de color un evento de clic que llama a jugadorPulsa pasandole ese color
 verde.addEventListener("click", function(){ jugadorPulsa(verde); });
 rojo.addEventListener("click", function(){ jugadorPulsa(rojo); });
 amarillo.addEventListener("click", function(){ jugadorPulsa(amarillo); });
